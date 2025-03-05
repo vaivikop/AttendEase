1c629e93572423b7631745eaae875cd062c4eb4d
vaivik <minecraftworld16764@gmail.com>
2025-03-05 18:46:58 +0530

    first commit

---
+++ app/layout.js
@@ -0,0 +1,29 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Attendease",
+  description: "An hackathon project by vaivik shah and pari vaghela",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-3xl font-bold">Welcome to AttendEase 🎯</h1>
+    </div>
+  );
+}
